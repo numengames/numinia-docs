122072a1436f8190e248365e2b2147d8e62aa7e3
DevStarlight <jesushuertaarrabal@gmail.com>
2025-01-29 19:09:50 +0100

    feat: Change github project URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/numengames/numinia/tree/main/',
+            'https://github.com/numengames/numinia-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/numengames/numinia/tree/main/',
+            'https://github.com/numengames/numinia-docs/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
